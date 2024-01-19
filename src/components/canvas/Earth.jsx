99505c157fc512e6e932f7541d48632cc181288e
Mayank Bangwal <rockmb972@gmail.com>
2024-01-19 18:22:12 +0530

    Contact section 1.0

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  return (
-    <div>Earth</div>
-  )
-}
+  return <div>Earth</div>;
+};
 
-export default Earth
+const EarthCanvas = () => {
+  return <Canvas></Canvas>;
+};
+
+export default Earth;
