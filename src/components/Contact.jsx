cb343b1c0311e89c2f53cf3006633b3b44abf77a
Mayank Bangwal <rockmb972@gmail.com>
2024-01-20 22:41:26 +0530

    final version

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,9 +15,44 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
 
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    emailjs
+      .send(
+        "service_agwured",
+        "template_1cydch4",
+        {
+          from_name: form.name,
+          to_name: "Mayank",
+          from_email: form.email,
+          to_email: "mykbgwl@gmail.com",
+          message: form.message,
+        },
+        "jSaLK3CWKHwqPthCc"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+          alert("Something went wrong.");
+        }
+      );
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
--- src/constants/index.js
+++ src/constants/index.js
@@ -142,24 +142,24 @@ import {
   const testimonials = [
     {
       testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
+        "I thought it was impossible to make a website as beautiful as our product, but Mayank proved me wrong.",
+      name: "Taylor Swift",
       designation: "CFO",
       company: "Acme Co",
       image: "https://randomuser.me/api/portraits/women/4.jpg",
     },
     {
       testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
+        "I've never met a web developer who truly cares about their clients' success like Mayank does.",
+      name: "Bruce Lee",
       designation: "COO",
       company: "DEF Corp",
       image: "https://randomuser.me/api/portraits/men/5.jpg",
     },
     {
       testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
+        "After Mayank optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lana Del Rey",
       designation: "CTO",
       company: "456 Enterprises",
       image: "https://randomuser.me/api/portraits/women/6.jpg",
